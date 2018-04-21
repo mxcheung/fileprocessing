12a359c65ddea4ad85a893703d640bcee69f17f3
max cheung <maxcheung@optusnet.com.au>
2018-04-22 07:47:54 +1000

    Tidy

--- src/main/java/au/com/maxcheung/futureclearer/flatfile/FlatFileReader.java
+++ src/main/java/au/com/maxcheung/futureclearer/flatfile/FlatFileReader.java
@@ -1,11 +1,14 @@
 package au.com.maxcheung.futureclearer.flatfile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.FixedLengthTokenizer;
 
 import au.com.maxcheung.futureclearer.model.FutureTransaction;
 
@@ -14,5 +17,7 @@ public interface FlatFileReader {
     List<FutureTransaction> read(String filename) throws UnexpectedInputException, ParseException, Exception;
 
     DefaultLineMapper<FutureTransaction> getLineMapper() throws IOException;
+
+    FixedLengthTokenizer getTokenizer(InputStream filePath) throws FileNotFoundException, IOException;
 
 }
--- src/main/java/au/com/maxcheung/futureclearer/flatfile/FlatFileReaderImpl.java
+++ src/main/java/au/com/maxcheung/futureclearer/flatfile/FlatFileReaderImpl.java
@@ -1,22 +1,17 @@
 package au.com.maxcheung.futureclearer.flatfile;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.LineMapper;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.FixedLengthTokenizer;
 import org.springframework.batch.item.file.transform.Range;
@@ -37,70 +32,40 @@ import au.com.maxcheung.futureclearer.model.FutureTransaction;
 @Service
 public class FlatFileReaderImpl extends BaseReader implements FlatFileReader {
 
+    private static final String FUTURE_FILESPEC_CSV = "future-filespec.csv";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(FlatFileReaderImpl.class);
 
-    private CsvReader csvReader;
-    private DefaultLineMapper<FutureTransaction> lineMapper;
-    private FlatFileItemReader<FutureTransaction> reader;
+    private final CsvReader csvReader;
+    private final DefaultLineMapper<FutureTransaction> lineMapper;
+    private final FlatFileItemReader<FutureTransaction> reader;
 
     public FlatFileReaderImpl() throws IOException {
-        this.csvReader = new CsvReader(FlatFileSpec.class);
+        csvReader = new CsvReader(FlatFileSpec.class);
         lineMapper = getLineMapper();
         reader = (FlatFileItemReader<FutureTransaction>) getReader(lineMapper);
     }
 
     @Override
-    public List<FutureTransaction> read(String filename)
-            throws UnexpectedInputException, ParseException, Exception {
+    public List<FutureTransaction> read(String filename) throws UnexpectedInputException, ParseException, Exception {
         getLineMapper();
         reader.setResource(new FileSystemResource(getFile(filename)));
         reader.open(new ExecutionContext());
-        List<FutureTransaction> rows = readData();
+        List<FutureTransaction> rows = readData(reader);
         LOGGER.info("Processed file : {}, rows : {}.", filename, rows.size());
         return rows;
     }
 
-    public ItemReader<FutureTransaction> getReader(LineMapper<FutureTransaction> futureLineMapper) {
-        FlatFileItemReader<FutureTransaction> flatFileItemReader = new FlatFileItemReader<>();
-        flatFileItemReader.setLineMapper(futureLineMapper);
-        return flatFileItemReader;
+    @Override
+    public DefaultLineMapper<FutureTransaction> getLineMapper() throws IOException {
+        InputStream fileSpecIS = new ClassPathResource(FUTURE_FILESPEC_CSV).getInputStream();
+        DefaultLineMapper<FutureTransaction> mapper = new DefaultLineMapper<FutureTransaction>();
+        mapper.setLineTokenizer(getTokenizer(fileSpecIS));
+        mapper.setFieldSetMapper(getFieldSetMapper());
+        return mapper;
     }
 
     @Override
-    public DefaultLineMapper<FutureTransaction> getLineMapper() throws IOException {
-        InputStream cpfileSpec = new ClassPathResource("future-filespec.csv").getInputStream();
-        lineMapper = new DefaultLineMapper<FutureTransaction>();
-        lineMapper.setLineTokenizer(getTokenizer(cpfileSpec));
-        lineMapper.setFieldSetMapper(getFieldSetMapper());
-        return lineMapper;
-    }
-
-    
-    private BeanWrapperFieldSetMapper<FutureTransaction> getFieldSetMapper() {
-        BeanWrapperFieldSetMapper<FutureTransaction> wrapper = new BeanWrapperFieldSetMapper<FutureTransaction>();
-        wrapper.setTargetType(FutureTransaction.class);
-        return wrapper;
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> List<T> readData() throws Exception {
-        List<T> txns = new ArrayList<>();
-        T t = null;
-        do {
-            t = (T) reader.read();
-            if (t != null) {
-                txns.add(t);
-            }
-        } while (t != null);
-        return txns;
-
-    }
-
-    private File getFile(String fileName) {
-        return new File(fileName);
-    }
-
-
     public FixedLengthTokenizer getTokenizer(InputStream filePath) throws FileNotFoundException, IOException {
         List<FlatFileSpec> rows = csvReader.readCsv(filePath);
         FixedLengthTokenizer tokenizer = new FixedLengthTokenizer();
